7bc6609ada10003cff0ff1246a206c59758f5549
JuneHyung <cjh951114@gmail.com>
2023-03-15 17:24:58 +0900

    feat : default layout 변경 및 login 레이아웃 디자인 작업중

--- develop/frontend/bumpy-nuxt3/layouts/default.vue
+++ develop/frontend/bumpy-nuxt3/layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <p>default layout</p>
-    <Menu></Menu>
+  <div class="default-layout">
     <NuxtPage />
   </div>
 </template>
 
 <script setup>
-import Menu from "~/components/Menu.vue";
+import Menu from '~/components/Menu.vue';
 </script>
+<style lang="scss" scoped>
+.default-layout {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+</style>
