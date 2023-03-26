8c94273277bcec10b169e3bad4552c4d8a2cf385
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-03-26 20:01:13 +0900

    feat : 버튼관련 css 분리 및 로그인 화면 일부 수정 적용

--- develop/frontend/bumpy-nuxt3/pages/main.vue
+++ develop/frontend/bumpy-nuxt3/pages/main.vue
@@ -126,113 +126,11 @@
     </div>
   </main>
 </template>
-<script setup lang="ts">
+<script setup>
 definePageMeta({
   layout: 'main-layout',
 });
 </script>
 <style lang="scss">
-.main-page {
-  width: 940px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 30px;
-  background-color: green;
-}
-.content-title {
-  color: #fb8c00;
-}
-.content-wrap-box {
-  background-color: #fff;
-  border-radius: 20px;
-  border: none;
-  padding: 16px;
-  margin: 8px 0;
-}
 
-.activity-meal-wrap-box {
-  display: flex;
-  flex-direction: row;
-
-  .activity-wrap-box,
-  .meal-wrap-box {
-    flex-direction: column;
-
-    .activity-title-box,
-    .meal-title-box {
-      display: flex;
-      /* flex-wrap: wrap; */
-      /* flex-direction: row; */
-      .activity-title,
-      .meal-title {
-        /* justify-self: flex-start; */
-        align-content: flex-start;
-      }
-      .activity-date,
-      .meal-date {
-        justify-content: center;
-        align-items: center;
-        justify-self: flex-end;
-        align-self: end;
-      }
-    }
-  }
-  .activity-wrap-box {
-    flex: 3;
-    margin-right: 8px;
-    .calendar {
-      flex: 1 1 200px;
-      height: 200px;
-      margin-top: 8px;
-      border: 1px solid black;
-    }
-  }
-  .meal-wrap-box {
-    flex: 9;
-    margin-left: 8px;
-  }
-}
-
-.last-activity-wrap-box {
-  display: flex;
-  flex-direction: row;
-  .last-activity-list {
-    flex: 3;
-    .activity-title-box {
-      display: flex;
-      align-items: center;
-    }
-  }
-  .last-activity-chart-box {
-    display: flex;
-    flex-direction: column;
-    flex: 9;
-    padding: 8px;
-    border: 1px solid black;
-    .last-activity-chart-info {
-      display: flex;
-      flex: 1;
-      flex-direction: row;
-      justify-content: center;
-
-      .chart-info-item {
-        flex: 3;
-        justify-content: center;
-        align-items: center;
-        height: 40px;
-        &:nth-child(2) {
-          border-right: 2px solid black;
-          border-left: 2px solid black;
-        }
-        /* .chart-info-item-title {
-        } */
-      }
-    }
-    .last-activity-chart {
-      border: 1px solid black;
-      height: 100px;
-    }
-  }
-}
 </style>
