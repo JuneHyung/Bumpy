229345b40c229415c7101f48a244f1aeb48d8cbb
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-03-26 21:37:03 +0900

    faet : background image 추가

--- develop/frontend/bumpy-nuxt3/layouts/default.vue
+++ develop/frontend/bumpy-nuxt3/layouts/default.vue
@@ -8,10 +8,13 @@
 import Menu from '~/components/Menu.vue';
 </script>
 <style lang="scss" scoped>
+
 .default-layout {
   display: flex;
   height: 100vh;
   justify-content: center;
   align-items: center;
+  background: url('@/assets/images/background-image.png') no-repeat center center;
+  background-size: cover;
 }
 </style>
