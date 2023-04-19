ecc213ec1442cf0c60dc1b46d91f8522cdb819ec
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-04-19 14:06:11 +0900

    feat : Calendar 스토리 및 컴포넌트 추가, icon문제 해결필요

---
+++ develop/frontend/bumpy-nuxt3/components/Calendar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <FullCalendar ref="calendar" :options="calendarOptions"></FullCalendar>
+  </div>
+</template>
+<script setup lang="ts">
+import {Ref, ref, onMounted} from 'vue';
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+// import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+const calendar:Ref  = ref(null);
+
+interface EventListItem{
+  title:string,
+  date: string,
+}
+type EventList = EventListItem[];
+
+interface Props {
+  list: EventList;
+}
+const props = defineProps<Props>();
+
+const calendarOptions = ref({
+  plugins: [
+    dayGridPlugin,
+    // timeGridPlugin,
+    interactionPlugin, // needed for dateClick
+  ],
+  headerToolbar: {
+    left: 'prevYear prev',
+    center: 'title',
+    right:'next nextYear',
+  },
+
+  footerToolbar:{
+    left:'',
+    center: 'today',
+    right:'',
+  },
+  initialView: 'dayGridMonth', // alternatively, use the `events` setting to fetch from a feed
+  // editable: false,
+  selectable: true,
+  events: props.list,
+  dayMaxEvents: 3,
+  // contentHeight: 100
+  // selectMirror: false,
+  // dayMaxEvents: false,
+  // weekends: false,
+  // select: this.handleDateSelect,
+  // eventClick: this.handleEventClick,
+  // eventsSet: handleEvents,
+  /* you can update a remote database when these fire:
+  eventAdd:
+  eventChange:
+  eventRemove:
+*/});
+
+</script>
+<style scoped>
+
+
+</style>
