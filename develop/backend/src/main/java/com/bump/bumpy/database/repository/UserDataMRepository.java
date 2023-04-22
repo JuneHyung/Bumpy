735e1e77d5b4d6783602430a68aea8171e012620
PataB <irr2000@paran.com>
2023-04-22 09:57:18 +0900

    feat : [DB] DB 설정 및 entity 연결

--- develop/backend/src/main/java/com/bump/bumpy/database/repository/UserDataMRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bump.bumpy.database.repository;
-
-import com.bump.bumpy.database.entity.UserData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDataMRepository extends JpaRepository<UserData, Integer> {
-}
