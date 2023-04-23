d0488fa00ebf36c87aebd75134666808649a2682
JuneHyung <cjh951114@gmail.com>
2023-04-23 16:26:53 +0900

    feat : Textarea input init & story작성

---
+++ develop/frontend/bumpy-nuxt3/components/form/TextareaInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="input-wrap-box">
+    <label>
+      <textarea
+        :value="data.value"
+        :placeholder="data.placeholder"
+        :autofocus="data.autofocus"
+        :cols="data.cols"
+        :rows="data.rows"
+        :maxlength="data.maxlength"
+        :minlength="data.minlength"
+        :disabled="data.disabled"
+        :readonly="data.readonly"
+        class="input-textarea"
+      ></textarea>
+    </label>
+  </div>
+</template>
+<script setup lang="ts">
+interface inputTextarea {
+  value?: string;
+  placeholder?: string;
+  autofocus?: boolean;
+  cols?: number;
+  rows?: number;
+  maxlength?: number;
+  minlength?: number;
+  disabled?: boolean;
+  readonly?: boolean;
+}
+
+interface Props {
+  data: inputTextarea;
+}
+const props = defineProps<Props>();
+</script>
+<style scoped></style>
