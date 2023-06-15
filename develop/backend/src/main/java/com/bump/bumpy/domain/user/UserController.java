7b287893fcdadbf0d4a3224e1b8a471e75859986
hjlee <hjlee@egap.co.kr>
2023-06-15 18:16:24 +0900

    feat : [User] 회원정보 수정, 탈퇴, 비밀번호 유효검사 추가

--- develop/backend/src/main/java/com/bump/bumpy/domain/user/UserController.java
+++ develop/backend/src/main/java/com/bump/bumpy/domain/user/UserController.java
@@ -1,15 +1,25 @@
 package com.bump.bumpy.domain.user;
 
+import com.bump.bumpy.domain.user.dto.UserUpdateDto;
+import com.bump.bumpy.util.dto.PasswordDto;
 import com.bump.bumpy.util.dto.ResultMap;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.bump.bumpy.util.funtion.FieldValueUtil.getUserId;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -20,26 +30,34 @@ public class UserController {
 
     @Operation(summary = "진입 확인용 API", description = "")
     @PostMapping("/check")
-    public ResponseEntity<ResultMap> check() {
-        String userId = getUserId();
-        return userService.check();
+    public ResponseEntity<ResultMap> entranceCheck(@RequestBody PasswordDto request) {
+        return userService.entranceCheck(request);
     }
 
     @Operation(summary = "비밀번호 유효성검사", description = "")
     @PostMapping("/password/check")
-    public ResponseEntity<ResultMap> insert() {
-        return userService.passwordCheck();
+    public ResponseEntity<ResultMap> insert(@RequestBody @Valid PasswordDto request, BindingResult result) {
+        if(result.hasErrors())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResultMap("message", result.getAllErrors().get(0).getDefaultMessage()));
+        return ResponseEntity.ok(new ResultMap("message", "OK"));
     }
 
     @Operation(summary = "사용자정보 업데이트", description = "")
-    @PostMapping("/update")
-    public ResponseEntity<ResultMap> update() {
-        return userService.update();
+    @PutMapping("/update")
+    public ResponseEntity<ResultMap> update(@RequestBody @Valid UserUpdateDto request) {
+        if(request.getPassword() != null) {
+            PasswordDto passwordDto = new PasswordDto();
+            passwordDto.setPassword(request.getPassword());
+            userService.updatePassword(passwordDto);
+        }
+
+        return userService.update(request);
     }
 
     @Operation(summary = "회원 탈퇴", description = "")
     @DeleteMapping("/delete")
-    public ResponseEntity<ResultMap> delete() {
+    public ResponseEntity<ResultMap> delete(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
+        new CookieClearingLogoutHandler("JSESSIONID").logout(request, response, authentication);
         return userService.delete();
     }
 }
--- develop/backend/src/main/java/com/bump/bumpy/domain/user/UserService.java
+++ develop/backend/src/main/java/com/bump/bumpy/domain/user/UserService.java
@@ -1,18 +1,80 @@
 package com.bump.bumpy.domain.user;
 
+import com.bump.bumpy.database.entity.usr.UsrMUsr;
+import com.bump.bumpy.database.repository.usr.UsrMUsrRepository;
+import com.bump.bumpy.domain.user.dto.UserUpdateDto;
+import com.bump.bumpy.util.dto.PasswordDto;
 import com.bump.bumpy.util.dto.ResultMap;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+
+import static com.bump.bumpy.util.funtion.FieldValueUtil.getUserId;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    public ResponseEntity<ResultMap> check() { return ResponseEntity.ok(new ResultMap()); }
 
-    public ResponseEntity<ResultMap> passwordCheck() { return ResponseEntity.ok(new ResultMap()); }
+    private final UsrMUsrRepository usrMUsrRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public ResponseEntity<ResultMap> update() { return ResponseEntity.ok(new ResultMap()); }
+    public ResponseEntity<ResultMap> entranceCheck(PasswordDto request) {
+        UsrMUsr user = usrMUsrRepository.findByUserId(getUserId());
 
-    public ResponseEntity<ResultMap> delete() { return ResponseEntity.ok(new ResultMap()); }
+        // null check
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ResultMap());
+        }
+
+        if(passwordEncoder.matches(request.getPassword(), user.getPassword()))
+            return ResponseEntity.status(HttpStatus.OK).build();
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResultMap());
+    }
+
+    public ResponseEntity<ResultMap> update(UserUpdateDto request) {
+
+        // update
+        UsrMUsr user = usrMUsrRepository.findByUserId(getUserId());
+        if(user == null) {
+            throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
+        }
+
+        user = request.updateEntity(user);
+
+        usrMUsrRepository.save(user);
+
+        return ResponseEntity.ok(new ResultMap("message", "OK"));
+    }
+
+    public ResponseEntity<ResultMap> delete() {
+        UsrMUsr user = usrMUsrRepository.findByUserId(getUserId());
+
+        usrMUsrRepository.delete(user);
+
+        return ResponseEntity.ok(new ResultMap("message", "OK"));
+    }
+
+    public void updatePassword(@Valid PasswordDto passwordDto) {
+        UsrMUsr user = usrMUsrRepository.findByUserId(getUserId());
+
+        // null check
+        if (user == null) {
+            throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
+        }
+
+        // Encoding Password
+        user.setPassword(passwordEncoder.encode(passwordDto.getPassword()));
+
+        // update
+        usrMUsrRepository.save(user);
+    }
 }
