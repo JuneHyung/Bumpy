b7bdf869aa776bbd5b37a0c436fd499a14c1324b
JUNEHYUNG\USER <cjh951114@gmail.com>
2023-08-16 15:30:03 +0900

    fix: 전체레이아웃 & 패스워드체크 버튼 수정

--- develop/frontend/bumpy-nuxt3/pages/user/passwordCheck.vue
+++ develop/frontend/bumpy-nuxt3/pages/user/passwordCheck.vue
@@ -3,8 +3,10 @@
     <div class="password-check-wrap-box content-wrap-box">
       <CustomIcon iconName="mdiLockCheck" style="width:60px; height:60px;"></CustomIcon>
       <h2 class="bp-my-lg">Enter Your Password</h2>
+      <div>
       <PasswordInput :data="password"/>
-      <button class="bp-my-lg" @click="checkPassword" type="button">Check</button>
+      <button class="bp-my-lg short-filled-button" @click="checkPassword" type="button" style="margin: 1rem auto;">Check</button>
+    </div>
     </div>
   </main>
 </template>
@@ -22,7 +24,6 @@ const password = ref({value: '',})
 const checkPassword = async () => {
   await userStore.checkPassword({password: password.value.value})
   if(userStore.getIsPass()) router.push({name: 'user-userPage'})
-
 }
 </script>
 
