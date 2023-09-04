55b7533f1bc56df2f89c535b02f9051a4acc4dec
hjlee <hjlee@egap.co.kr>
2023-09-04 13:33:07 +0900

    feat : cache 기능 추가

--- develop/backend/src/main/java/com/bump/bumpy/util/funtion/YoutubeService.java
+++ develop/backend/src/main/java/com/bump/bumpy/util/funtion/YoutubeService.java
@@ -2,7 +2,10 @@ package com.bump.bumpy.util.funtion;
 
 import com.bump.bumpy.util.dto.youtube.ResponseKeywordSearchDto;
 import com.bump.bumpy.util.dto.youtube.SearchListKeywordDto;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -13,6 +16,7 @@ import java.util.Map;
 public class YoutubeService {
     private static final String API_KEY = "AIzaSyDkzCviTKEBN-xue3AUWJBOesyq59UQq3k";
 
+    @Cacheable(value = "searchListKeyword", key = "#keyword")
     public ResponseKeywordSearchDto searchListKeyword(String keyword){
         /**
          * GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=[YOUR_API_KEY] HTTP/1.1
@@ -47,6 +51,11 @@ public class YoutubeService {
         }
 
         return responseKeywordSearchDto;
+    }
+
+    @CacheEvict(value = "searchListKeyword", allEntries = true)
+    @Scheduled(fixedRateString = "${caching.evictTTL}")
+    public void flushCache() {
     }
 
 }
