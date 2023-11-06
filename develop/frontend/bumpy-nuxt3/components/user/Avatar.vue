b8216644e4766a0b83f242aec25b20f915c2e465
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-11-06 10:30:14 +0900

    fix : UserInfo Avatar 수정

--- develop/frontend/bumpy-nuxt3/components/user/Avatar.vue
+++ develop/frontend/bumpy-nuxt3/components/user/Avatar.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="avatar-wrap-box">
-    <img src="~/assets/images/beam.png" class="avatar-image" />
+    <!-- <img src="~/assets/images/beam.png" class="avatar-image" /> -->
+    <CustomIcon iconName="mdiAccountCircle" class="avatar-image"></CustomIcon>
   </div>
 </template>
 <script setup lang="ts">
+import CustomIcon from '../icon/CustomIcon.vue';
 interface Props {
   name: string;
 }
